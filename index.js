baa502f29614ab33338e3c4f0c98d6e5415c96fa
ivarmlee1 <ivarr5@gmail.com>
2017-06-12 22:28:08 -0700

    update

--- index.js
+++ index.js
@@ -3,29 +3,8 @@ import bodyParser from 'body-parser';
 import alexaVerifier from 'alexa-verifier';
 import http from 'http';
 
-setInterval(function() {
-  http.get('http://alexathisdayinhistory.herokuapp.com');
-}, 400000); // every 4 minutes
-
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-function requestVerifier(req, res, next) {
-  console.log('request made it to middleware');
-  console.log(req);
-  alexaVerifier(
-    req.headers.signaturecertchainurl,
-    req.headers.signature,
-    req.rawBody,
-    function verificationCallback(err) {
-      if (err) {
-          res.status(401).json({ message: 'Verification Failure', error: err });
-      } else {
-          next();
-      }
-    }
-  );
-}
 
 app.use(bodyParser.json({
     verify: function getRawBody(req, res, buf) {
@@ -44,15 +23,14 @@ app.post('/thisdayinhistory', requestVerifier, (req, res) => {
         "shouldEndSession": true,
         "outputSpeech": {
           "type": "SSML",
-          "ssml": "<speak> I'm digging into my historical data banks! </speak>"
+          "ssml": "<speak> I'm digging into my historical data banks </speak>"
         }
       }
     });
   } else if (req.body.request.type === 'SessionEndedRequest') {
     // no response sent for this request.type
     console.log('Alexa session ended', req.body.request.reason);
-  } else if (req.body.request.type === 'IntentRequest' &&
-           req.body.request.intent.name === 'ThisDayInHistory') {
+  } else if (req.body.request.type === 'IntentRequest' && req.body.request.intent.name === 'ThisDayInHistory') {
     res.json({
       "version": "1.0",
       "response": {
@@ -63,9 +41,31 @@ app.post('/thisdayinhistory', requestVerifier, (req, res) => {
         }
       }
     });
+  } else {
+    console.log('what\'s going on')
   }
-
 });
+
+function requestVerifier(req, res, next) {
+  console.log('request made it to middleware');
+  console.log(req);
+  alexaVerifier(
+    req.headers.signaturecertchainurl,
+    req.headers.signature,
+    req.rawBody,
+    function verificationCallback(err) {
+      if (err) {
+          res.status(401).json({ message: 'Verification Failure', error: err });
+      } else {
+          next();
+      }
+    }
+  );
+}
+
+setInterval(() => {
+  http.get('http://alexathisdayinhistory.herokuapp.com');
+}, 400000); // every 4 minutes
 
 app.listen(PORT, () => {
   console.log(`listening on... ${PORT}`);
